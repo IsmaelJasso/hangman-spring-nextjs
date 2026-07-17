421e6e14bb6ab2e03e17426a6a597bc3297cf979
Ismael Jasso <197644360+IsmaelJasso@users.noreply.github.com>
2026-07-17 01:39:15 -0500

    Clean Spring Boot project setup in api subfolder

---
+++ hangman-api/src/main/java/com/ismael/hangman/HangmanApiApplication.java
@@ -0,0 +1,13 @@
+package com.ismael.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanApiApplication.class, args);
+	}
+
+}
---
+++ hangman-api/src/test/java/com/ismael/hangman/HangmanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismael.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
